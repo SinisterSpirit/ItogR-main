5c45e121054519e25be74f6c7dc3ff28b0e255a9
Drago <dragoko12@gmail.com>
2023-02-13 15:01:34 +0300

    sird commit

--- KR/Program.cs
+++ KR/Program.cs
@@ -1,7 +1,33 @@
 ﻿string[] mass = { "год", "человек", "время", "дело", "дом", "2", "0_o" };
 int lenM = mass.Length;
 wrArr(mass);
+string[] mass2 = searEl(mass);
+wrArr(mass2);
 
+string[] searEl(string[] mass)
+{
+    int count = 0;
+    for (int i = 0; i < lenM; i++)
+    {
+        if (mass[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+
+    count--;
+
+    string[] mass2 = new string[count];
+    for (int i = 0; i < lenM; i++)
+    {
+        if (mass[i].Length <= 3)
+        {
+            mass2[count] = mass[i];
+            count--;
+        }
+    }
+    return mass2;
+}
 
 void wrArr(string[] mass)
 {
