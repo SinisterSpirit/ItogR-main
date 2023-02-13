7fc0d798c96c93b05f5f3b4c4c11dd0d55fa0932
Drago <dragoko12@gmail.com>
2023-02-13 14:37:27 +0300

    second commit

--- KR/Program.cs
+++ KR/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] mass = { "год", "человек", "время", "дело", "дом", "2", "0_o" };
+int lenM = mass.Length;
+wrArr(mass);
+
+
+void wrArr(string[] mass)
+{
+    Console.Write("[");
+    for (int i = 0; i < lenM; i++)
+    {
+        Console.Write(mass[i]);
+        if (i < lenM - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.WriteLine("]");
+}
